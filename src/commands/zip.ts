84f7df402e5bc60876adaf03a86efd7710534ed6
Luiz Gustavo Freire Gama <eu@guto.dev>
2020-09-01 22:32:33 -0300

    chore: add lint-staged

--- src/commands/zip.ts
+++ src/commands/zip.ts
@@ -1,22 +1,30 @@
-import {Command, flags} from '@oclif/command'
-import zip from '../handlers/zip/zip.handler'
+import { Command, flags } from '@oclif/command';
+import zip from '../handlers/zip/zip.handler';
 
 export default class Zip extends Command {
-  static description = 'Generate project zip file into .releases directory based on latest tag'
+  static description =
+    'Generate project zip file into .releases directory based on latest tag';
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: 'h' }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'file name'}),
+    name: flags.string({ char: 'n', description: 'file name' }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f', description: 'forces rewriting existing file with same name'}),
+    force: flags.boolean({
+      char: 'f',
+      description: 'forces rewriting existing file with same name',
+    }),
     // Dry run
-    dryRun: flags.boolean({char: 'd', description: 'simulates action, do not generate or modify any file in project'})
-  }
+    dryRun: flags.boolean({
+      char: 'd',
+      description:
+        'simulates action, do not generate or modify any file in project',
+    }),
+  };
 
-  async run() {
-    const {flags} = this.parse(Zip)
-
-    return await zip(this)(flags)
+  async run(): Promise<boolean> {
+    const { flags } = this.parse(Zip);
+    await zip(this)(flags);
+    return true;
   }
 }
--- src/handlers/zip/zip.handler.ts
+++ src/handlers/zip/zip.handler.ts
@@ -1,20 +1,35 @@
-import { exec } from 'child_process'
-import { FlagsInterface } from '../../interfaces/flags.interface'
+import { exec } from 'child_process';
+import { FlagsInterface } from '../../interfaces/flags.interface';
 import Command from '@oclif/command';
 // import * as fs from 'fs';
 // import { callbackify } from 'util';
 
 const zip = (command: Command) => async (flags: FlagsInterface) => {
-    const { name, force, dryRun } = flags || {}
-    if (dryRun) {
-        if (force) {
-            command.log('Rewriting')
-        } else {
-            command.log('Writing')
-        }
-        return
+  const { name, force, dryRun } = flags || {};
+  if (dryRun) {
+    if (force) {
+      command.log('Rewriting');
+    } else {
+      command.log('Writing');
     }
-    exec(name ? `bash -c "ztag() { mkdir -p ./.releases/; echo "Generating...\`pwd\`/.releases/${name}.zip"; git archive HEAD --format=zip --prefix="$wd" --output="./.releases/${name}.zip" && echo "Done!"; }; ztag` : `bash -c "ztag() { mkdir -p ./.releases/; wd=$(basename \`pwd\`); tag=$(git describe --tags --abbrev=0); echo "Generating...\`pwd\`/.releases/$wd-$tag"; git archive $tag --format=zip --prefix="$wd" --output="./.releases/$wd-$tag.zip" && echo "Done!"; }; ztag` , (error: any, stdout: string, stderr: string) => { if (error) { command.log(`error: ${error.message}`); return; } if (stderr) { command.log(`stderr: ${stderr}`); return; } command.log(`${stdout}`); })
+    return;
+  }
+  exec(
+    name
+      ? `bash -c "ztag() { mkdir -p ./.releases/; echo "Generating...\`pwd\`/.releases/${name}.zip"; git archive HEAD --format=zip --prefix="$wd" --output="./.releases/${name}.zip" && echo "Done!"; }; ztag`
+      : `bash -c "ztag() { mkdir -p ./.releases/; wd=$(basename \`pwd\`); tag=$(git describe --tags --abbrev=0); echo "Generating...\`pwd\`/.releases/$wd-$tag"; git archive $tag --format=zip --prefix="$wd" --output="./.releases/$wd-$tag.zip" && echo "Done!"; }; ztag`,
+    (error: any, stdout: string, stderr: string) => {
+      if (error) {
+        command.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        command.log(`stderr: ${stderr}`);
+        return;
+      }
+      command.log(`${stdout}`);
+    }
+  );
 };
 
-export default zip
+export default zip;
